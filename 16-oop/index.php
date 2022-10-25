28ec5bb65b6ba121a7877e6098b0e91de25cff5a
ofac <ofaczero@gmail.com>
2022-10-25 11:53:41 -0500

    chore: new folder oop

---
+++ 16-oop/index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP (Object Oriented Programming)</title>
+    <script src="public/js/tailwind-3.2.1.js"></script>
+</head>
+<body class="bg-gradient-to-t
+           from-indigo-900 
+           to-indigo-500 
+             overflow-hidden
+             min-h-screen">
+    
+    <main class="my-20 
+                 mx-auto
+                 max-w-lg 
+                 p-5 
+                 border-2 
+               border-indigo-400 
+               bg-white/5 
+                 rounded">
+        <h1 class="m-5 
+                   text-center 
+                   text-2xl 
+                 text-white 
+                   text-opacity-50 
+                   font-medium">
+            OOP (Object Oriented Programming)
+        </h1>
+        <section class="text-white 
+                          max-h-[590px] 
+                          overflow-y-scroll
+                          text-opacity-50
+                          p-5
+                          mt-10
+                          rounded">
+            <ol class="flex 
+                       flex-col 
+                       justify-center 
+                       items-center 
+                       gap-1">
+                <li class="text-xl 
+                         text-white/70">
+                    <a href="01-class.php" class="bg-black 
+                                        bg-opacity-40 
+                                        w-96 
+                                        p-4 
+                                        text-xl 
+                                        flex 
+                                        text-white/70 
+                                        rounded
+                                        transition
+                                        hover:bg-opacity-50
+                                        hover:scale-105">
+                                        01 - Class
+                    </a>
+                </li>
+                <li class="text-xl 
+                         text-white/70">
+                    <a href="02-construct.php" class="bg-black 
+                                        bg-opacity-40 
+                                        w-96 
+                                        p-4 
+                                        text-xl 
+                                        flex 
+                                        text-white/70 
+                                        rounded
+                                        transition
+                                        hover:bg-opacity-50
+                                        hover:scale-105">
+                                        02 - Construct
+                    </a>
+                </li>
+                <li class="text-xl 
+                         text-white/70">
+                    <a href="03-private.php" class="bg-black 
+                                        bg-opacity-40 
+                                        w-96 
+                                        p-4 
+                                        text-xl 
+                                        flex 
+                                        text-white/70 
+                                        rounded
+                                        transition
+                                        hover:bg-opacity-50
+                                        hover:scale-105">
+                                        03 - Private
+                    </a>
+                </li>
+                <li class="text-xl 
+                         text-white/70">
+                    <a href="04-collaboration.php" class="bg-black 
+                                        bg-opacity-40 
+                                        w-96 
+                                        p-4 
+                                        text-xl 
+                                        flex 
+                                        text-white/70 
+                                        rounded
+                                        transition
+                                        hover:bg-opacity-50
+                                        hover:scale-105">
+                                        04 -  Collaboration
+                    </a>
+                </li>
+                <li class="text-xl 
+                         text-white/70">
+                    <a href="05-parameters.php" class="bg-black 
+                                        bg-opacity-40 
+                                        w-96 
+                                        p-4 
+                                        text-xl 
+                                        flex 
+                                        text-white/70 
+                                        rounded
+                                        transition
+                                        hover:bg-opacity-50
+                                        hover:scale-105">
+                                        05 - Parameters
+                    </a>
+                </li>
+                <li class="text-xl 
+                         text-white/70">
+                    <a href="06-extends.php" class="bg-black 
+                                        bg-opacity-40 
+                                        w-96 
+                                        p-4 
+                                        text-xl 
+                                        flex 
+                                        text-white/70 
+                                        rounded
+                                        transition
+                                        hover:bg-opacity-50
+                                        hover:scale-105">
+                                        06 - Extends
+                    </a>
+                </li>
+                <li class="text-xl 
+                         text-white/70">
+                    <a href="07-overwhite-method.php" class="bg-black 
+                                        bg-opacity-40 
+                                        w-96 
+                                        p-4 
+                                        text-xl 
+                                        flex 
+                                        text-white/70 
+                                        rounded
+                                        transition
+                                        hover:bg-opacity-50
+                                        hover:scale-105">
+                                        07 - Overwrite Method
+                    </a>
+                </li>
+                <li class="text-xl 
+                         text-white/70">
+                    <a href="08-overwrite-construct.php" class="bg-black 
+                                        bg-opacity-40 
+                                        w-96 
+                                        p-4 
+                                        text-xl 
+                                        flex 
+                                        text-white/70 
+                                        rounded
+                                        transition
+                                        hover:bg-opacity-50
+                                        hover:scale-105">
+                                        08 - Overwrite Construct
+                    </a>
+                </li>
+                <li class="text-xl 
+                         text-white/70">
+                    <a href="09-class-abstract.php" class="bg-black 
+                                        bg-opacity-40 
+                                        w-96 
+                                        p-4 
+                                        text-xl 
+                                        flex 
+                                        text-white/70 
+                                        rounded
+                                        transition
+                                        hover:bg-opacity-50
+                                        hover:scale-105">
+                                        09 - Class Abstract
+                    </a>
+                </li>
+                <li class="text-xl 
+                         text-white/70">
+                    <a href="10-method-abstract.php" class="bg-black 
+                                        bg-opacity-40 
+                                        w-96 
+                                        p-4 
+                                        text-xl 
+                                        flex 
+                                        text-white/70 
+                                        rounded
+                                        transition
+                                        hover:bg-opacity-50
+                                        hover:scale-105">
+                                        10 - Method Abstract
+                    </a>
+                </li>
+            </ol>
+            
+        </section>
+    </main>
+    
+</body>
+</html>
